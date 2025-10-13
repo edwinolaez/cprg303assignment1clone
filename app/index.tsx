02620dda4bf47e4722bf2d82f8d1b1f7810d1a2f
Edwin Olaez <edwinolaez@yahoo.com>
2025-10-13 17:20:53 -0600

    updated index and layout file

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,12 @@
-import { Text, View } from "react-native";
+import HomePage from "@/components/home-page";
+import { View } from "react-native";
+
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      style={{flex: 1,}}>
+      <HomePage />
     </View>
   );
 }
