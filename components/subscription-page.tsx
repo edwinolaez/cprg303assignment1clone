391ad752bf6e72d8a6ce9bbd21347938342aadd7
Monica Leung <Manki.Leung@edu.sait.ca>
2025-10-16 16:14:03 -0600

    added 3-dots-icon, and completed subscription-page

--- components/subscription-page.tsx
+++ components/subscription-page.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { FlatList, Image, Platform, StatusBar, StyleSheet, Text, View } from 'react-native';
+import { FlatList, Image, Platform, ScrollView, StatusBar, StyleSheet, Text, View } from 'react-native';
 import BellIcon from './Icons/bellIcon';
+import DotsIcon from './Icons/dots';
 import HomeIcon from './Icons/homeIcon';
 import SearchIcon from './Icons/searchIcon';
 import ShortsIcon from './Icons/shortsIcon';
@@ -31,19 +32,20 @@ export default function subscriptionPage() {
     resizeMode: 'contain',
   },
     header: {
-    flexDirection:'row',
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'space-between',
-    padding: 10,
+    justifyContent: 'space-between', 
+    paddingHorizontal: 10,           
+    paddingVertical: 10,
     borderBottomWidth: 1,
     borderBottomColor: '#ddd',
   },
-    tabs: {flexDirection: 'row', justifyContent: 'space-around', flex: 1},
+    tabs: { flexDirection: 'row', justifyContent: 'space-between', flex: 1},
     Icon:{
       height: 30,
       width: 30,
       resizeMode: 'contain',
-      marginHorizontal: 8,
+      marginHorizontal: 5,
     },
     leftSection:{ 
       height: 30,
@@ -60,6 +62,8 @@ export default function subscriptionPage() {
     bottomNav: {
       flexDirection: 'row',
       justifyContent: 'space-around',
+      borderTopWidth: 1,
+      borderTopColor: '#ddd',
     },
     label: {
       fontSize: 12,
@@ -86,6 +90,7 @@ export default function subscriptionPage() {
     profileContainer: {
       marginTop: 10,
       marginLeft: 10,
+      marginBottom: 5,
     },
     profileItem: {
       marginRight: 12,
@@ -96,6 +101,106 @@ export default function subscriptionPage() {
       height: 60,
       borderRadius: 30,
     },
+    filterRow: {
+      flexDirection: "row",
+      paddingVertical: 10,
+      paddingHorizontal: 10,
+
+    },
+    filterButton: {
+      marginRight: 10,
+    },
+    filterText: {
+      fontSize: 14,
+      paddingVertical: 6,
+      paddingHorizontal: 14,
+      borderRadius: 10,
+      backgroundColor: "#f2f2f2",
+      fontWeight: "bold",
+    },
+    videoCard: {
+      marginBottom: 15,
+    },
+    thumbnail: {
+      width: "100%",
+      height: 230,
+      // borderRadius: 10,
+    },
+    durationTag: {
+      position: "absolute",
+      bottom: 6,
+      right: 6,
+      backgroundColor: "rgba(0,0,0,0.8)",
+      paddingHorizontal: 4,
+      paddingVertical: 2,
+      borderRadius: 4,
+    },
+    durationText: {
+      color: "white",
+      fontSize: 15,
+      fontWeight: "bold",
+    },
+    videoInfo: {
+      flexDirection: "row",
+      padding: 10,
+    },
+    channelIcon: {
+      width: 36,
+      height: 36,
+      borderRadius: 18,
+      marginRight: 10,
+    },
+    textContainer: {
+      flex: 1,
+    },
+    title: {
+      fontSize: 18,
+      fontWeight: "bold",
+      color: "black",
+    },
+    titleHorizontal: {
+      fontSize: 14,
+      fontWeight: "bold",
+      color: "black",
+    },
+    subtitle: {
+      fontSize: 12,
+      color: "gray",
+      marginTop: 1,
+    },
+    videoCardHorizontal: {
+      marginLeft: 15,
+      marginBottom: 10,
+      marginTop: 10,
+      width: 260,
+      
+    },
+    sectionTitle: {
+      fontSize: 20,
+      fontWeight: "bold",
+      marginLeft: 15,
+      
+    },
+    thumbnailHorizontal: {
+      width: "100%",
+      height: 145,
+      borderRadius: 10,
+    },
+    dotsStyle:{ 
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'flex-start',
+    },
+    rightBar: {
+      position: 'absolute',
+      right: 0,        
+      top: 0,          
+      bottom: 0,       
+      width: 40,       
+      backgroundColor: 'white', 
+    }
+
 
 
 });
@@ -126,7 +231,7 @@ export default function subscriptionPage() {
       id: "2",
       thumbnail: require("../assets/images/video2.jpg"),
       profile: require("../assets/images/profile2.jpg"),
-      title: "What Are The Vibes In Vancouver | Real Kyper & Bourne",
+      title: "The Vibes In Vancouver | Real Kyper & Bourne",
       channel: "Sportsnet",
       views: "120K views",
       time: "1 day ago",
@@ -145,49 +250,141 @@ export default function subscriptionPage() {
     
   ];
 
+  let reversedVideos = videos.reverse();
+
 
     
     return (
+    // Top Nav
     <View  style={styles.container}>
       <View style={styles.header}>
 
-            <View style={styles.tabs}>
-                  <View style={styles.leftSection}><YoutubeLogo /></View>
-                  <View style={styles.rightSection}>
-                    <View style={styles.Icon}><Image style={styles.castIconStyle}
-                      source={require("../assets/images/CastIcon.png")}
-                    /></View>
-                    <View style={styles.Icon}><BellIcon /></View>
-                    <View style={styles.Icon}><SearchIcon /></View>
-                  </View>
-                  
+        <View style={styles.tabs}>
+              <View style={styles.leftSection}><YoutubeLogo /></View>
+              <View style={styles.rightSection}>
+                <View style={styles.Icon}><Image style={styles.castIconStyle}
+                  source={require("../assets/images/CastIcon.png")}
+                /></View>
+                <View style={styles.Icon}><BellIcon /></View>
+                <View style={styles.Icon}><SearchIcon /></View>
               </View>
-            </View>
+              
+          </View>
+        </View>
 
             {/* Main Content */}
-            {/* ---------- profile ROW ---------- */}
-            <View style={styles.profileContainer}>
-               <FlatList
-                  horizontal
-                  showsHorizontalScrollIndicator={false}
-                  data={stories}
-                  renderItem={({ item }) => {
-                    // Trim the name if it’s longer than 5 characters
-                    const displayName =
-                      item.name.length > 5 ? item.name.slice(0, 5) + "..." : item.name;
+            <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
+              {/* ---------- profile ROW ---------- */}
+              <View style={styles.profileContainer}>
+                <FlatList
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    data={stories}
+                    renderItem={({ item }) => {
+                      // Trim the name if it’s longer than 5 characters
+                      const displayName =
+                        item.name.length > 5 ? item.name.slice(0, 5) + "..." : item.name;
 
-                    return (
-                      <View style={styles.profileItem}>
-                        <Image source={item.profile} style={styles.profileImage} />
-                        <Text >{displayName}</Text>
-                      </View>
-                    );
-                  }}
-                />
-            </View>
+                      return (
+                        <View style={styles.profileItem}>
+                          <Image source={item.profile} style={styles.profileImage} />
+                          <Text >{displayName}</Text>
+                        </View>
+                      );
+                    }}
+                  />
+                <View style={styles.rightBar}><Text style={{fontSize: 13, textAlign: 'center', color: 'blue', fontWeight: 'bold', marginTop: 23}}>All</Text></View>
+              </View>
 
+              {/* ---------- FILTER BUTTONS ---------- */}
+              <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+                <View style={styles.filterRow}>
+                  {["All", "Today", "Videos", "Shorts", "Live", "Podcasts"].map((text, index) => (
+                    <View key={index} style={styles.filterButton}>
+                      <Text
+                        style={[
+                          styles.filterText,
+                          index === 0 && { color: "white", backgroundColor: "black" },
+                        ]}
+                      >
+                        {text}
+                      </Text>
+                    </View>
+                  ))}
+                </View>
+              </ScrollView>
+              
 
+              {/* ---------- VIDEO LIST ---------- */}
+              {/* Horizontal */}
+              <Text style={styles.sectionTitle}>Most relevant</Text>
+              <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+                {videos.map((item) => (
+                <View key={item.id} style={styles.videoCardHorizontal}>
+                  <View>
+                    <Image source={item.thumbnail} style={styles.thumbnailHorizontal} />
+                    <View style={styles.durationTag}>
+                      <Text style={styles.durationText}>{item.duration}</Text>
+                    </View>
+                  </View>
 
+                  <View style={styles.videoInfo}>
+                    <View style={styles.textContainer}>
+                      <div style={styles.dotsStyle}>
+                        <Text style={styles.titleHorizontal} numberOfLines={2}>
+                          {item.title}
+                        </Text>
+                        <div style={{marginLeft: 10, width: 10}}>
+                          <DotsIcon />
+                        </div>
+                        
+                      </div>
+                      
+                      <Text style={styles.subtitle}>
+                        {item.channel}
+                      </Text>
+                      <Text style={styles.subtitle}>
+                        {item.views} • {item.time}
+                      </Text>
+                    </View>
+                  </View>
+                </View>
+              ))}
+
+              {/* Vertical */}
+              </ScrollView>
+              {videos.slice().reverse().map((item) => (
+                <View key={item.id} style={styles.videoCard}>
+                  <View>
+                    <Image source={item.thumbnail} style={styles.thumbnail} />
+                    <View style={styles.durationTag}>
+                      <Text style={styles.durationText}>{item.duration}</Text>
+                    </View>
+                  </View>
+
+                  <View style={styles.videoInfo}>
+                    <Image source={item.profile} style={styles.channelIcon} />
+                    <View style={styles.textContainer}>
+                      <div style={styles.dotsStyle}>
+                        <Text style={styles.title} numberOfLines={2}>
+                          {item.title}
+                        </Text>
+                        <div style={{marginLeft: 10}}>
+                          <DotsIcon />
+                        </div>
+                        
+                      </div>
+                      
+                      <Text style={styles.subtitle}>
+                        {item.channel} • {item.views} • {item.time}
+                      </Text>
+                    </View>
+                  </View>
+                </View>
+              ))}
+            </ScrollView>
+
+            {/* Bottom Navigation */}
             <View style={styles.bottomNav}>
               {/* Home */}
               <View style={styles.tab}>
@@ -223,30 +420,6 @@ export default function subscriptionPage() {
                 <Text style={styles.label}>You</Text>
               </View>
             </View>
-      
-      <YoutubeLogo />
-      <BellIcon />
-
-      <HomeIcon selected={true}/>
-
-      <SubscriptionsIcon selected={false} />
-      <SearchIcon />
-      <div ><Image style={styles.ProfileIcon}
-        source={require("../assets/images/MyProfile.png")}
-      /></div>
-      <div ><Image style={styles.castIconStyle}
-        source={require("../assets/images/CastIcon.png")}
-      /></div>
-      <div ><Image style={styles.ProfileIcon}
-        source={require("../assets/images/PlusIcon.png")}
-      /></div>
-
-      <div ><Image style={styles.ProfileIcon}
-        source={require("../assets/images/profile1.jpg")}
-      /></div>
-      <div ><Image style={styles.video}
-        source={require("../assets/images/video1.jpg")}
-      /></div>
 
   </View>
     
